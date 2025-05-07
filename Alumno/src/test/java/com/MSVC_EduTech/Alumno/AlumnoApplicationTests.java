c57ec5255506a0e143cd65c6ab42ba7530b4a8e9
Renato416 <re.rojasc@duocuc.cl>
2025-05-07 12:07:59 -0400

    init commit

---
+++ Alumno/src/test/java/com/MSVC_EduTech/Alumno/AlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.Alumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
