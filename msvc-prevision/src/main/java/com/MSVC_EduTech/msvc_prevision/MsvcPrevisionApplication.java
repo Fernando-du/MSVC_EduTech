74c154db8910f21eec3e287cfa60605686661944
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-14 11:51:36 -0400

    Avance 14/5 v.2

---
+++ msvc-prevision/src/main/java/com/MSVC_EduTech/msvc_prevision/MsvcPrevisionApplication.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.msvc_prevision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcPrevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcPrevisionApplication.class, args);
+	}
+
+}
---
+++ msvc-prevision/src/test/java/com/MSVC_EduTech/msvc_prevision/MsvcPrevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.msvc_prevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcPrevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
