3f90dcfdfbf59bde6845e8c03018fc1236949e3b
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-15 11:39:54 -0400

    15/05

--- Alumno/src/main/java/com/MSVC_EduTech/Alumno/services/AlumnoServiceImpl.java
+++ Alumno/src/main/java/com/MSVC_EduTech/Alumno/services/AlumnoServiceImpl.java
@@ -2,7 +2,6 @@ package com.MSVC_EduTech.Alumno.services;
 
 import com.MSVC_EduTech.Alumno.exceptions.AlumnoException;
 import com.MSVC_EduTech.Alumno.models.Alumno;
-import com.MSVC_EduTech.Alumno.models.Prevision;
 import com.MSVC_EduTech.Alumno.repositories.AlumnoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,9 +27,8 @@ public class AlumnoServiceImpl {
     }
     @Override
     public Alumno save(Alumno alumno) {
-        try {
-            Prevision prevision = this.previsionClientRest.findById()
-        }
+        return alumnoRepository.save(alumno);
     }
+
 
 }
