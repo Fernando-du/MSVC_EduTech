ff500e9895abba6747be32c92ae25c8e534c4f52
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-15 12:16:29 -0400

    15/05 v.2

--- Alumno/src/main/java/com/MSVC_EduTech/Alumno/controllers/AlumnoController.java
+++ Alumno/src/main/java/com/MSVC_EduTech/Alumno/controllers/AlumnoController.java
@@ -1,4 +1,43 @@
 package com.MSVC_EduTech.Alumno.controllers;
 
+import com.MSVC_EduTech.Alumno.models.Alumno;
+import com.MSVC_EduTech.Alumno.services.AlumnoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/alumnos")
+@Validated
 public class AlumnoController {
+
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @GetMapping
+    public ResponseEntity<List<Alumno>> findAll(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(this.alumnoService.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Alumno> findById(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(this.alumnoService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Alumno> create(@Valid @RequestBody Alumno alumno) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(this.alumnoService.save(alumno));
+    }
+
+
 }
