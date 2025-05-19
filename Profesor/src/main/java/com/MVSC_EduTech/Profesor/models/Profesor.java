262fd997c2c8e55ac4d6ef85444bc9062915d39a
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-19 10:48:31 -0400

    19/05 V.3 prof

---
+++ Profesor/src/main/java/com/MVSC_EduTech/Profesor/models/Profesor.java
@@ -0,0 +1,14 @@
+package com.MVSC_EduTech.Profesor.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "profesores")
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+public class Profesor {
+
+
+}
