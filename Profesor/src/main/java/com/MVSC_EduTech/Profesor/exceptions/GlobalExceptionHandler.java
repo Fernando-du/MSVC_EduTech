8a97738df7d1b5097c0ad39fcc86f6ee406dae12
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-22 12:20:35 -0400

    22/05 v.3

--- Profesor/src/main/java/com/MVSC_EduTech/Profesor/exceptions/GlobalExceptionHandler.java
+++ Profesor/src/main/java/com/MVSC_EduTech/Profesor/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.MVSC_EduTech.Profesor.exceptions;
 
 import com.MVSC_EduTech.Profesor.dtos.ErrorDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -11,5 +16,16 @@ public class GlobalExceptionHandler {
 
     private ErrorDTO createErrorDTO(int status, Date date, Map<String, String> errorMap){
         ErrorDTO errorDTO = new ErrorDTO();
+
+        errorDTO.setStatus(status);
+        errorDTO.setDate(date);
+        errorDTO.setErrors(errorMap);
+
+        return errorDTO;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<ErrorDTO> handleValidationFiels (MethodArgumentNotValidException exception){
+        Map<String, String> errorMap = new HashMap<>();
     }
 }
