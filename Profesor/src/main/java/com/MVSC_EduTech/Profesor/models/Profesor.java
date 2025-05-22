fb8aa7b7fc5f27f25595bc56d373c18962814941
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-22 11:39:57 -0400

    22/05

--- Profesor/src/main/java/com/MVSC_EduTech/Profesor/models/Profesor.java
+++ Profesor/src/main/java/com/MVSC_EduTech/Profesor/models/Profesor.java
@@ -1,14 +1,44 @@
 package com.MVSC_EduTech.Profesor.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "profesores")
 @Getter @Setter @ToString
 @NoArgsConstructor @AllArgsConstructor
 public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "El campo run profesor no puede estar vacio")
+    @Pattern(regexp = "\\d{1,8}-[\\dKk]", message = "El formato del run profesor debe ser XXXXXXXX-X")
+    private String runProfesor;
+
+    @Column(nullable = false)
+    @NotBlank(message = "El campo nombre profesor no puede estar vacio")
+    private String nombres;
+
+    @Column(nullable = false)
+    @NotBlank(message = "El campo apellido profesor no puede estar vacio")
+    private String apellidos;
+
+    @Column(nullable = false)
+    @NotBlank(message = "El campo correo profesor no puede estar vacio")
+    private String correo;
+
+    @Column(nullable = false)
+    @NotBlank(message = "El campo fecha contratacion no puede estar vacio")
+    private Date fechaContratacion;
+
+
 
 
 }
