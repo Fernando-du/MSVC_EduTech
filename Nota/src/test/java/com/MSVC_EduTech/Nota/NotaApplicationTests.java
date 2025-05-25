09d956cc668ec3461ea3db7b520c2430211f1c92
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-25 15:59:24 -0400

    25/05 Nota añadido

---
+++ Nota/src/test/java/com/MSVC_EduTech/Nota/NotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.Nota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
