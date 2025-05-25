4464f9d79ddde0fce39c8e42d72e726c4541a484
Fernando-du <fernan.martineza@duocuc.cl>
2025-05-25 15:51:06 -0400

    25/05 Curso añadido

---
+++ Curso/src/main/java/com/MSVC_EduTech/Curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.Curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
---
+++ Curso/src/test/java/com/MSVC_EduTech/Curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MSVC_EduTech.Curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
