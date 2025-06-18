c9cccc56493fa901ce071cff1b7afb77936f8299
Renato416 <re.rojasc@duocuc.cl>
2025-06-18 10:33:17 -0400

    conexion

--- Alumno/src/main/java/com/MSVC_EduTech/Alumno/AlumnoApplication.java
+++ Alumno/src/main/java/com/MSVC_EduTech/Alumno/AlumnoApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlumnoApplication {
+public class
+AlumnoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlumnoApplication.class, args);
