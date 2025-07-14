b4bf11a7089a6ea46b35a8b0e7054666cf028529
Fernando-du <fernan.martineza@duocuc.cl>
2025-07-14 04:54:35 -0400

    14/7 Bases OpenApi hechas 4:54

--- Alumno/src/main/java/com/MSVC_EduTech/Alumno/controllers/AlumnoController.java
+++ Alumno/src/main/java/com/MSVC_EduTech/Alumno/controllers/AlumnoController.java
@@ -1,8 +1,15 @@
 package com.MSVC_EduTech.Alumno.controllers;
 
+import com.MSVC_EduTech.Alumno.dtos.ErrorDTO;
 import com.MSVC_EduTech.Alumno.models.Alumno;
 import com.MSVC_EduTech.Alumno.services.AlumnoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +32,30 @@ public class AlumnoController {
     @GetMapping
     @Operation(summary = "Endpoint que obtiene todos los alumnos",
                description = "Este endpoint devuelve en un List todo los alumnos que se encuentran en la base de datos")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Operacion de extracion de Alumnos exitosa"
+            )
+    })
     public ResponseEntity<List<Alumno>> findAll(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(this.alumnoService.findAll());
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Endpoint que devuelve un alumno por id",
+               description = "Endpoint que va devolver un Alumno.class al momento de buscarlo por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Obtencion por id correcta"),
+            @ApiResponse(responseCode = "404", description = "Error cuando el alumno con cierto id no existe",
+            content = @Content(
+                    mediaType = "applicacion/json", schema = @Schema(implementation = ErrorDTO.class)
+            ))
+    })
+    @Parameters(value = {
+    @Parameter(name = "id", description = "Primary Key - Entidad Alumno", required = true)
+    })
     public ResponseEntity<Alumno> findById(@PathVariable Long id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -38,6 +63,25 @@ public class AlumnoController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Endpoint que guarda a un Alumno",
+            description = "Endpoint que permite capturar un elemento Alumno.class y lo guarda dentro de nuestra base de datos"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Creacion Existosa"),
+            @ApiResponse(responseCode = "404", description = "Algun elemento de msvc no se encuentra", content = @Content(
+                    mediaType = "applicacion/json", schema = @Schema(implementation = ErrorDTO.class)
+            )),
+            @ApiResponse(responseCode = "409", description = "El elemento que intentas crear ya existe", content = @Content(
+                    mediaType = "applicacion/json", schema = @Schema(implementation = ErrorDTO.class)
+            ))
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Estructura de datos que se me permite realizar la creación de un alumno",
+            content = @Content(
+                    mediaType = "applicacion/json", schema = @Schema(implementation = ErrorDTO.class)
+            )
+    )
     public ResponseEntity<Alumno> create(@Valid @RequestBody Alumno alumno) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -45,6 +89,17 @@ public class AlumnoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Endpoint para eliminar un alumno por id",
+            description = "Este endpoint elimina un alumno de la base de datos a través de su id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Eliminación exitosa"),
+            @ApiResponse(responseCode = "404", description = "El alumno con ese ID no existe", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)
+            ))
+    })
+    @Parameter(name = "id", description = "ID del alumno a eliminar", required = true)
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         alumnoService.deleteById(id);
         return ResponseEntity
@@ -52,6 +107,27 @@ public class AlumnoController {
                 .build();
     }
     @PutMapping
+    @Operation(
+            summary = "Endpoint para actualizar un alumno por ID",
+            description = "Este endpoint permite actualizar los datos de un alumno existente mediante su ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Actualización exitosa del alumno"),
+            @ApiResponse(responseCode = "404", description = "El alumno no se encuentra en la base de datos", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Datos inválidos o incompletos", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)
+            ))
+    })
+    @Parameters(value = {
+            @Parameter(name = "id", description = "ID del alumno que se desea actualizar", required = true)
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Objeto Alumno con los nuevos datos para la actualización",
+            required = true,
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Alumno.class))
+    )
     public ResponseEntity<Alumno> updateById(@PathVariable Long id, @RequestBody Alumno alumno) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
